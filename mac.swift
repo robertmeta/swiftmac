98e7aa9830f10b3f71c2fd2145c693e7c1f3e571
Robert Melton <robert@robertmelton.com>
2023-03-06 00:33:35 -0500

    Most major features in

--- mac.swift
+++ mac.swift
@@ -3,45 +3,92 @@ import Darwin
 import Foundation
 import AppKit
 
+// Supporting Classes
+class DelegateHandler: NSObject, NSSpeechSynthesizerDelegate {
+
+    func speechSynthesizer(_ sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
+        speaker.startSpeaking(backLog.pop())
+    }
+}
+
+class Backlog {
+    private var myBacklog: String = ""
+    private let queue = DispatchQueue(label: "org.emacspeak.mac.swift")
+
+    func clear()  {
+        queue.async {
+            self.myBacklog = ""
+        }
+    }
+    
+    func push(with: String) {
+        queue.async {
+            // Access myBacklog here
+            self.myBacklog += with
+        }
+    }
+
+    func pop() -> String {
+        var result: String = ""
+        queue.sync {
+            // Access myBacklog here
+            result = self.myBacklog
+        }
+        self.clear()
+        return result
+    }
+}
+
 // Global Constants
 let version = "0.1"
 let name = "mac.swift"
-let audioIconCmd  = "a "
-let ttsSayCmd     = "tts_say "
-let playSoundCmd  = "p "
-let queueCmd      = "q "
-let stopCmd       = "s "
-let dispatchCmd   = "d "
-let speechSynthesizer = NSSpeechSynthesizer()
-@MainActor
-var speechQueue: String = ""
-@MainActor
-let queue = DispatchQueue(label: "com.example.myqueue", attributes: .concurrent)
 
-let semaphore = DispatchSemaphore(value: 1)
-
+let speaker = NSSpeechSynthesizer()
+let backLog = Backlog()
+let dh = DelegateHandler()
 
 // default values
 let voice = NSSpeechSynthesizer.VoiceName(rawValue: "com.apple.speech.synthesis.voice.Alex")
-speechSynthesizer.setVoice(voice)
+speaker.setVoice(voice)
+speaker.delegate = dh
 
 // Entry point and main loop
 func main() async {
     while let l = readLine() {
-        print(await isolateCommand(line: l))
-        debugPrint(await stripSpecialEmbeds(l))
-        if l.hasPrefix(audioIconCmd) {
+        let cmd = await isolateCommand(line: l)
+        debugPrint(cmd)
+        switch cmd { 
+        case "a":
             await playAudioIcon(line: l)
-        } else if l.hasPrefix(playSoundCmd) {
+        case "p":
             await playSound(line: l)
-        } else if l.hasPrefix(ttsSayCmd) {
+        case "tts_say": 
             await ttsSay(line: l)
-        } else if l == "exit" {
+        case "exit":
             await exitApp()
-        } else {
+        case "q":
+            await queueSpeaker(line: l)
+        case "d":
+            await dispatchSpeaker()
+        case "s":
+            await stopSpeaker()
+        default:
             await unknownLine(line: l)
         }
     }
+}
+
+func stopSpeaker() async {
+    speaker.stopSpeaking()
+}
+
+func dispatchSpeaker () async {
+    speaker.startSpeaking(backLog.pop())
+}
+
+func queueSpeaker (line: String) async {
+    let p = await isolateParams(line: line)
+    backLog.push(with: " "+p)
 }
 
 // Does the same thing as "p " so route it over to playSound
@@ -52,30 +99,26 @@ func playAudioIcon(line: String) async {
 
 func playSound(line: String) async {
     print("Playing sound: "+line)
-    let p = await isolateParams(line: line, cmd: playSoundCmd)
+    let p = await isolateParams(line: line)
     let soundURL = URL(fileURLWithPath: p)
     NSSound(contentsOf: soundURL, byReference: true )?.play()
 }
 
 func ttsSay(line: String) async {
     print("ttsSay: "+line)
-    let p = await isolateParams(line: line, cmd: ttsSayCmd)
+    let p = await isolateParams(line: line)
     await say(what: p, interupt: true)
     
 }
 
 func say(what: String, interupt: Bool) async {
     if interupt {
-        speechSynthesizer.startSpeaking(what)
+        speaker.startSpeaking(what)
     } else {
-        if speechSynthesizer.isSpeaking {
-            queue.async {
-                semaphore.wait()
-                speechQueue += " "+what
-                semaphore.signal()
-            }
+        if speaker.isSpeaking {
+            backLog.push(with: what)
         } else {
-            speechSynthesizer.startSpeaking(what)
+            speaker.startSpeaking(what)
         }
     }
 }
@@ -84,11 +127,14 @@ func unknownLine(line: String) async {
     debugPrint("Unknown command: "+line)
 }
 
+// TODO: make signals call this as well
 func exitApp() async {
     print("Exiting "+name)
     exit(0)
 }
 
+// TODO: handle these, so far I know it uses voice
+// and echo
 func stripSpecialEmbeds(_ s: String) async -> String {
     let specialEmbedRegexp = #"\[\{.*?\}\]"#
     return s.replacingOccurrences(of: specialEmbedRegexp, with: "", options: .regularExpression)    
@@ -100,10 +146,11 @@ func isolateCommand(line: String) async -> String {
         cmd.replaceSubrange(firstIndex..<cmd.endIndex, with: "")
         return cmd
     }
-    return ""
+    return cmd
 }
 
-func isolateParams(line: String, cmd: String) async -> String {
+func isolateParams(line: String) async -> String {
+    let cmd = await isolateCommand(line: line)+" "
     var params = line.replacingOccurrences(of: cmd, with: "")
     params = params.trimmingCharacters(in: .whitespacesAndNewlines)
     if params.hasPrefix("{") && params.hasSuffix("}") {

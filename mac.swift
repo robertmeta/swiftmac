3b84d139b8a4fa0e2686a484a7a8350c3570d15d
Robert Melton <robert@robertmelton.com>
2023-03-14 00:17:53 -0400

    Closer by the day, stuck on addDictionary...
    This add dictionary is messing with me to an
    annoying degree, get an error -50 (IO error)
    when trying to laod my custom dictionary, I
    am going to try doing it in plain text for
    now.

--- mac.swift
+++ mac.swift
@@ -1,19 +1,46 @@
 #!/usr/bin/env swift
+/*
+ This software is licensed under the MIT license found at the end of
+ the file.
 
+ Motivations for why it was written and goals are also at end of
+ the file.
+
+ TODO:
+ - Finish python based test driver to validate functionality
+ - Add signal handling (control-c, etc)
+ - Add handling of voice changes (most of the plumbing is ready)
+ - Add echo handling via custom NSSpeechSynthesizer with echo effect
+ - Add custom dictionary objects to pronounce things rather than using
+  string manipulation, might need a seperate speaker per class
+ - (maybe) disable built-in voice on the fly as speaking to make it
+  require zero configuration to use (no double speaking)  
+ - (maybe) Switch to AVFoundation and utterances
+  - Makes it easier to handle adding speech in flight
+  - All speech comes with its own voice and specifics like speed
+  - Very much fits how emacspeak generates speech
+ */
 import AVFoundation
 import AppKit
 import Darwin
 import Foundation
 
-// Supporting Classes
+// This delegate class lets us continue speaking with queued data
+// after a speech chunk is completed
 class DelegateHandler: NSObject, NSSpeechSynthesizerDelegate {
-  func speechSynthesizer(_ sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
-    speaker.startSpeaking(ss.popBacklog())
-  }
+    func speechSynthesizer(
+        _ sender: NSSpeechSynthesizer,
+        didFinishSpeaking finishedSpeaking: Bool
+    ) {
+        speaker.startSpeaking(ss.popBacklog())
+    }
 }
 
+// Due to being fully async, handling the state is a bit of a pain,
+// we have to store it all in a class and gate access to it, the good
+// news is the only syncronise bits are on reading the data out. 
 class StateStore {
-    private var backlog: String = ""
+  private var backlog: String = ""
   private var rate: Int = 275
   private var sayRate: Int = 330
   private var speechVolume: Float = 1
@@ -22,7 +49,8 @@ class StateStore {
   private var voice: String = "Alex"
   private var splitCaps: Bool = false
   private var charScale: Double = 1.2
-  private let queue = DispatchQueue(label: "org.emacspeak.server.mac.state", qos: .userInteractive)
+  private let queue = DispatchQueue(
+    label: "org.emacspeak.server.mac.state", qos: .userInteractive)
 
   func setVoice(voice: String) {
     queue.async {
@@ -57,8 +85,13 @@ class StateStore {
   }
 }
 
-func playPureTone(frequencyInHz: Int, amplitude: Float, durationInMillis: Int) async {
-  let toneQueue = DispatchQueue(label: "org.emacspeak.server.mac.tone", qos: .userInteractive)
+// Generates a tone in pure swift
+func playPureTone(
+  frequencyInHz: Int, amplitude: Float,
+  durationInMillis: Int
+) async {
+  let toneQueue = DispatchQueue(
+    label: "org.emacspeak.server.mac.tone", qos: .userInteractive)
   let semaphore = DispatchSemaphore(value: 1)
   toneQueue.async {
     let audioPlayer = AVAudioPlayerNode()
@@ -70,7 +103,8 @@ func playPureTone(frequencyInHz: Int, amplitude: Float, durationInMillis: Int) a
 
     guard
       let format = AVAudioFormat(
-        commonFormat: AVAudioCommonFormat.pcmFormatFloat32, sampleRate: Double(sampleRateHz),
+        commonFormat: AVAudioCommonFormat.pcmFormatFloat32,
+        sampleRate: Double(sampleRateHz),
         channels: AVAudioChannelCount(1), interleaved: false)
     else {
       return
@@ -78,21 +112,29 @@ func playPureTone(frequencyInHz: Int, amplitude: Float, durationInMillis: Int) a
 
     audioEngine.connect(audioPlayer, to: mixer, format: format)
 
-    let numberOfSamples = AVAudioFrameCount((Float(durationInMillis) / 1000 * sampleRateHz))
+    let numberOfSamples = AVAudioFrameCount(
+      (Float(durationInMillis)
+        / 1000 * sampleRateHz))
 
-    guard let buffer = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: numberOfSamples) else {
+    guard
+      let buffer = AVAudioPCMBuffer(
+        pcmFormat: format,
+        frameCapacity: numberOfSamples)
+    else {
       return
     }
     buffer.frameLength = numberOfSamples
 
     let channels = UnsafeBufferPointer(
       start: buffer.floatChannelData, count: Int(format.channelCount))
-    let floats = UnsafeMutableBufferPointer<Float>(start: channels[0], count: Int(numberOfSamples))
+    let floats = UnsafeMutableBufferPointer<Float>(
+      start: channels[0], count: Int(numberOfSamples))
 
     let angularFrequency = Float(frequencyInHz * 2) * .pi
 
     for i in 0..<Int(numberOfSamples) {
-      let waveComponent = sinf(Float(i) * angularFrequency / sampleRateHz)
+      let waveComponent =
+        sinf(Float(i) * angularFrequency / sampleRateHz)
       floats[i] = waveComponent * amplitude
     }
     do {
@@ -114,7 +156,7 @@ func playPureTone(frequencyInHz: Int, amplitude: Float, durationInMillis: Int) a
 }
 
 // Global Constants
-let version = "0.1"
+let version = "0.2"
 let name = "mac.swift"
 
 let speaker = NSSpeechSynthesizer()
@@ -122,11 +164,19 @@ let ss = StateStore()
 let dh = DelegateHandler()
 
 // default values
-let voice = NSSpeechSynthesizer.VoiceName(rawValue: "com.apple.speech.synthesis.voice.Alex")
+let voice = NSSpeechSynthesizer.VoiceName(
+  rawValue: "com.apple.speech.synthesis.voice.Alex")
 speaker.setVoice(voice)
 speaker.rate = 275
 speaker.volume = 1
 speaker.delegate = dh
+
+// let oai = NSSpeechSynthesizer.DictionaryKey(rawValue: "OpenAI")
+// debugPrint(oai)
+// let customPronunciations = [oai: "open A I"]
+// debugPrint(customPronunciations)
+// speaker.addSpeechDictionary(customPronunciations)
+
 
 // Entry point and main loop
 func main() async {
@@ -189,12 +239,12 @@ func ttsReset() async {
 }
 
 func sayVersion() async {
-    await say(what: "Running \(name) version \(version)", interupt: true)
+  await say(what: "Running \(name) version \(version)", interupt: true)
 }
 
 func sayLetter(line: String) async {
-    let letter = "S"
-    await say(what:"[[ltrl]]\(letter)[[norm]]", interupt: true)
+  let letter = "S"
+  await say(what: "[[ctrl]]\(letter)[[norm]]", interupt: true)
 }
 
 func saySilence(line: String) async {
@@ -202,11 +252,11 @@ func saySilence(line: String) async {
 }
 
 func ttsPause() async {
-  
+    speaker.pauseSpeaking(at: .immediateBoundary)
 }
 
 func ttsResume() async {
-  
+    speaker.continueSpeaking()
 }
 
 func ttsSetCharacterScale(line: String) async {
@@ -230,8 +280,11 @@ func ttsSyncState(line: String) async {
 }
 
 func playTone(line: String) async {
-    let toneVolume:Float = 1.0
-  await playPureTone(frequencyInHz: 440, amplitude: toneVolume, durationInMillis: 1000)
+  let toneVolume: Float = 1.0
+  await playPureTone(
+    frequencyInHz: 440,
+    amplitude:
+      toneVolume, durationInMillis: 1000)
 }
 
 func stopSpeaker() async {
@@ -290,20 +343,16 @@ func unknownLine(line: String) async {
   debugPrint("Unknown command: " + line)
 }
 
-// TODO: make signals call this as well
 func ttsExit() async {
   print("Exiting " + name)
   exit(0)
 }
 
-// TODO: handle these, so far I know it uses voice
-// and echo
-// voice is straightforward, call setVoice but
-// dealing with echo is trickier but possible with
-// a customized NSVoiceSynth
 func stripSpecialEmbeds(_ s: String) async -> String {
   let specialEmbedRegexp = #"\[\{.*?\}\]"#
-  return s.replacingOccurrences(of: specialEmbedRegexp, with: "", options: .regularExpression)
+  return s.replacingOccurrences(
+    of: specialEmbedRegexp,
+    with: "", options: .regularExpression)
 }
 
 func isolateCommand(line: String) async -> String {
@@ -331,3 +380,42 @@ func isolateParams(line: String) async -> String {
 }
 
 await main()
+
+/* MOTIVATIONS
+ This is a port of the wonderful mac server for emacspeak, the reasons
+ for the port are varied, but the key one was a lack of reliability
+ when running the mac server for long periods of time, which I suspect
+ is actually from the retention code deep in pyobjc but not sure.
+
+ Goals:
+ - Highly relibable (no memory leaks, no voiceover garble)
+ - Completely non-blocking (never lag emacs)
+ - Feature-complete (if emacspeak supports it we do)
+ - No dependandies except swift (meaning xcode)
+ - No install needed, compile on the fly with #!
+ - Take full advantage of built-in VoiiceOver features 
+*/
+
+
+/* LICENSE
+ Copyright 2023 Robert Melton
+
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation files
+ (the “Software”), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge,
+ publish, distribute, sublicense, and/or sell copies of the Software,
+ and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */

06314a5b5ee0ebdedc49fa61d76b78a7a24a968f
Robert Melton <robert@robertmelton.com>
2024-01-03 16:29:25 -0500

    how is q broken, my gosh:

--- test-server.py
+++ test-server.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import time
+from time import sleep
 
 # Check if the correct number of arguments was passed
 if len(sys.argv) != 2:
@@ -13,13 +14,13 @@ server = sys.argv[1]
 # Define the command to run as a list of strings
 command = [server]
 
-def sendit(process, cmd, wait):
-    
-
 # Open a subprocess and pipe data to its stdin
 with subprocess.Popen(command, stdin=subprocess.PIPE) as process:
     process.stdin.write(b"tts_say Testing Speech Server")
-    slee(1)
+    sleep(1)
     process.stdin.write(b"tts_say tts_say is working if you can hear this.\n")
+    process.stdin.flush()
+    time.sleep(3)
+    process.stdin.write(b"tts_say why is q broken is the question q q q\n")
     process.stdin.flush()
     time.sleep(3)

23284659ed5c27c707d90d4a9bc14db8c723a96e
Robert Melton <robert@robertmelton.com>
2024-01-13 20:33:35 -0500

    I hardcoded percent handling because VoiceOver isn't handling % well
    But, it is even stranger than that, like, it will read a certain
    number of them but not all of them, not going to dig too deeply
    as this bandaid seems to fix it and I am not sure that it will
    reoccur in the v2 stuff.... yey!

--- Sources/SwiftMacPackage/main.swift
+++ Sources/SwiftMacPackage/main.swift
@@ -4,7 +4,7 @@ import Darwin
 import Foundation
 
 /* Global Constants */
-let version = "1.0.4"
+let version = "1.0.5"
 let name = "swiftmac"
 let speaker = NSSpeechSynthesizer()
 let defaultRate: Float = 200
@@ -145,6 +145,7 @@ func replaceBasePuncs(_ line: String) -> String {
   #endif
   let l = replaceCore(line)
   return replaceCore(l)
+    .replacingOccurrences(of: "%", with: " percent ")
     .replacingOccurrences(of: "$", with: " dollar ")
 
 }

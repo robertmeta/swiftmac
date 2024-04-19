a00295141e64d9b258cc462018a953503fc52d85
Robert Melton <robert@robertmelton.com>
2024-04-19 17:28:48 -0400

    wip:

--- Sources/SwiftMacPackage/main.swift
+++ Sources/SwiftMacPackage/main.swift
@@ -4,6 +4,7 @@ import Darwin
 import Foundation
 import OggDecoder
 
+
 /* Globals */
 #if DEBUG
   let currentDate = Date()
@@ -14,7 +15,7 @@ import OggDecoder
 #else
   let debugLogger = Logger()  // No-Op
 #endif
-let version = "2.4.2"
+let version = "2.4.3"
 let name = "swiftmac"
 var ss = await StateStore()  // just create new one to reset
 let speaker = AVSpeechSynthesizer()

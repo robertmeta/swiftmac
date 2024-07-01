db5780c23c7ba1d067db712db2e9bb4672ea3992
Robert Melton <robert@robertmelton.com>
2024-07-01 12:56:32 -0400

    2.8.2:

--- Sources/SwiftMacPackage/main.swift
+++ Sources/SwiftMacPackage/main.swift
@@ -15,7 +15,7 @@ import OggDecoder
 #else
   let debugLogger = Logger()  // No-Op
 #endif
-let version = "2.8.1"
+let version = "2.8.2"
 let name = "swiftmac"
 var ss = await StateStore()  // just create new one to reset
 let speaker = AVSpeechSynthesizer()
